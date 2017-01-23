4af7674bad4a2b17372e5a3f8d2268873b3452dd
Mario Rossi <veontomo@gmail.com>
2017-01-23 16:45:13 +0100

    feat: set up project

---
+++ MailerController.pas
@@ -0,0 +1,56 @@
+unit MailerController;
+
+interface
+
+uses
+  MVCFramework, MVCFramework.Commons;
+
+type
+
+  [MVCPath('/')]
+  TMailerController = class(TMVCController)
+  public
+    [MVCPath('/')]
+    [MVCHTTPMethod([httpGET])]
+    procedure Index;
+
+    [MVCPath('/hellos/($FirstName)')]
+    [MVCHTTPMethod([httpGET])]
+    procedure GetSpecializedHello(const FirstName: String);
+  protected
+    procedure OnBeforeAction(Context: TWebContext; const AActionName: string; var Handled: Boolean); override;
+    procedure OnAfterAction(Context: TWebContext; const AActionName: string); override;
+  end;
+
+implementation
+
+uses
+  MVCFramework.Logger;
+
+procedure TMailerController.Index;
+begin
+  //use Context property to access to the HTTP request and response 
+  Render('Hello World');
+end;
+
+procedure TMailerController.GetSpecializedHello(const FirstName: String);
+begin
+  Render('Hello ' + FirstName);
+end;
+
+procedure TMailerController.OnAfterAction(Context: TWebContext; const AActionName: string);
+begin
+  { Executed after each action }
+  inherited;
+end;
+
+procedure TMailerController.OnBeforeAction(Context: TWebContext; const AActionName: string; var Handled: Boolean);
+begin
+  { Executed before each action
+    if handled is true (or an exception is raised) the actual
+    action will not be called }
+  inherited;
+end;
+
+
+end.
