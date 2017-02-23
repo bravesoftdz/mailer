6adc761438db271b2497abf963c21c94c6fc7aed
Mario Rossi <veontomo@gmail.com>
2017-02-23 13:02:13 +0100

    Map the properties from the front end request to a json object keys

--- FrontEndRequest.pas
+++ FrontEndRequest.pas
@@ -3,7 +3,7 @@ unit FrontEndRequest;
 interface
 
 uses
-  System.JSON, System.Generics.Collections, Attachment;
+  System.JSON, System.Generics.Collections, Attachment, REST.Json.Types;
 
 type
 
@@ -14,20 +14,25 @@ type
   const
     MSG_TOKEN = 'message';
     ATTACHMENTS_TOKEN = 'attachments';
-
   private
-    FOrigin: String;
-    FData: String;
-    // FAttachments: TObjectList<TAttachment>;
+    [JSONMarshalled(True)]
+    [JSonName('html')]
+    FHtml: String;
+
+    [JSONMarshalled(True)]
+    [JSonName('text')]
+    FText: String;
+
+    [JSONMarshalled(True)]
+    [JSonName('attachments')]
+    FAttachments: TObjectList<TAttachment>;
   public
     /// <summary> Constructor</summary>
-    /// <param name="Origin">origin of the request</param>
-    /// <param name="Data">data associated with the request. It is a json object
-    /// from which actually only a key 'message' is taken into account.</param>
-    constructor Create(const Origin: String; const aData: TJSonObject);
-    property Data: String read FData;
-    property Origin: String read FOrigin;
-    // property Attachments: TObjectList<TAttachment> read FAttachments;
+    /// <param name="aData">data associated with the request.</param>
+    constructor Create(const aText, anHtml: String; const Attachs: TObjectList<TAttachment>);
+    property Html: String read FHtml;
+    property Text: String read FText;
+    property Attachments: TObjectList<TAttachment> read FAttachments;
     function ToString(): String;
   end;
 
@@ -38,19 +43,12 @@ uses
 
 { TSimpleInputData }
 
-constructor TFrontEndRequest.Create(const Origin: String;
-  const aData: TJSonObject);
-var
-  msg: String;
-  val: TJsonValue;
+constructor TFrontEndRequest.Create(const aText, anHtml: String;
+  const Attachs: TObjectList<TAttachment>);
 begin
-  FOrigin := Origin;
-  if (aData <> nil) then
-  begin
-    val := aData.GetValue(MSG_TOKEN);
-    if val <> nil then
-      FData := val.Value;
-  end;
+  FText := aText;
+  FHtml := anHtml;
+  FAttachments := Attachs;
 end;
 
 function TFrontEndRequest.ToString: String;
@@ -58,10 +56,8 @@ var
   Builder: TStringBuilder;
 begin
   Builder := TStringBuilder.Create;
-  Builder.Append('destination: ');
-  Builder.Append(FOrigin);
-  Builder.Append(', data: ');
-  Builder.Append(FData);
+  Builder.Append(', html: ');
+  Builder.Append(FHtml);
   Result := Builder.ToString;
   Builder.DisposeOf;
 end;
