e32445876896e96d4f3be72843be719b0ae6f3fd
Mario Rossi <veontomo@gmail.com>
2017-03-23 16:42:38 +0100

    Implement the subscription

--- ActiveQueue/ActiveQueueModel.pas
+++ ActiveQueue/ActiveQueueModel.pas
@@ -3,13 +3,18 @@ unit ActiveQueueModel;
 interface
 
 uses
-  ActiveQueueResponce, SubscriptionData;
+  ActiveQueueResponce, SubscriptionData, System.Generics.Collections;
 
 type
   /// <summary>
   /// A model corresponding to the ActiveQueue controller.
   /// </summary>
   TActiveQueueModel = class
+  strict private
+  var
+    /// a dumb lock object
+    FLock: TObject;
+    SubscriptionMap: TDictionary<String, TSubscriptionData>;
   public
     /// <summary>Create a subscription </summary>
     /// <param name="Ip">ip of the computer from which the subscription request comes from</param>
@@ -18,6 +23,9 @@ type
     /// <summary> Cancel the subscription corrsponding to given ip</summary>
     /// <param name="Ip">Ip of the computer which subscription is to be cancelled</param>
     function CancelSubscription(const Ip: String): TActiveQueueResponce;
+
+    constructor Create();
+    destructor Destroy();
   end;
 
 implementation
@@ -28,9 +36,25 @@ uses
 
 function TActiveQueueModel.AddSubscription(const Ip: String;
   const data: TSubscriptionData): TActiveQueueResponce;
+var
+  dataCopy: TSubscriptionData;
 begin
-  /// stub
-  Result := TActiveQueueResponce.Create(True, 'your ip is ' + Ip + ', your port is ' + inttostr(data.Port));
+  TMonitor.Enter(FLock);
+  try
+    if SubscriptionMap.ContainsKey(Ip) then
+    begin
+      Result := TActiveQueueResponce.Create(False, 'your ip ' + Ip + ' is already subscribed, port ' + inttostr(SubscriptionMap[Ip].Port));
+    end
+    else
+    begin
+      dataCopy := TSubscriptionData.Create(data.Url, data.Port, data.Path);
+      SubscriptionMap.Add(Ip, dataCopy);
+      Result := TActiveQueueResponce.Create(True, 'your ip is ' + Ip + ', your port is ' + inttostr(data.Port));
+    end;
+  finally
+    TMonitor.Exit(FLock);
+  end;
+
 end;
 
 function TActiveQueueModel.CancelSubscription(
@@ -38,6 +62,19 @@ function TActiveQueueModel.CancelSubscription(
 begin
   Result := TActiveQueueResponce.Create(true, 'request to cancel your subscription (' + Ip + ') is accepted.');
 
+end;
+
+constructor TActiveQueueModel.Create;
+begin
+  FLock := TObject.Create;
+  SubscriptionMap := TDictionary<String, TSubscriptionData>.Create;
+end;
+
+destructor TActiveQueueModel.Destroy;
+begin
+  FLock.DisposeOf;
+  SubscriptionMap.Clear;
+  SubscriptionMap.DisposeOf;
 end;
 
 end.
