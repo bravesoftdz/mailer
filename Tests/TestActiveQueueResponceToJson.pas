ed78e377287f6daac1c97c9451a52b143857fbbf
Mario Rossi <veontomo@gmail.com>
2017-04-19 10:37:29 +0200

    Update tests

--- Tests/TestActiveQueueResponceToJson.pas
+++ Tests/TestActiveQueueResponceToJson.pas
@@ -10,34 +10,45 @@ type
   [TestFixture]
   TTestActiveQueueResponceToJson = class(TObject)
   public
-    [Setup]
-    procedure Setup;
-    [TearDown]
-    procedure TearDown;
     // Test suit for the serialization
     // Partition the input as follows
     // 1. status: true, false
     // 2. message length: 0, > 0
+    // 3. token length: 0, > 0
     [Test]
     // Cover
     // 1. status: true
     // 2. message length: > 0
-    [TestCase('Serialize with True and non-empty msg', 'True,a message')]
+    // 3. token length: > 0
+    [TestCase('Serialize with True and non-empty msg and token', 'True,a message,token')]
     // Cover
     // 1. status: false
     // 2. message length: > 0
-    [TestCase('Serialize with False and non-empty msg', 'False, some message')]
-    procedure SerializeNonEmptyMsg(const status: Boolean; const msg: String);
-    [Test]
+    // 3. token length: > 0
+    [TestCase('Serialize with False and non-empty msg and token', 'False, some message, a token')]
     // Cover
     // 1. status: true
     // 2. message length: 0
-    [TestCase('Serialize with True and empty msg', 'True')]
+    // 3. token length:  0
+    [TestCase('Serialize with True and empty msg and token', 'True,,')]
     // Cover
     // 1. status: false
     // 2. message length:  0
-    [TestCase('Serialize with False and empty msg', 'False')]
-    procedure SerializeJustMessageIsSetToEmpty(const status: Boolean);
+    // 3. token length:  0
+    [TestCase('Serialize with False and empty msg and token', 'False,,')]
+    // Cover
+    // 1. status: true
+    // 2. message length: > 0
+    // 3. token length:  0
+    [TestCase('Serialize with True and non-empty msg and empty token', 'True,some message,')]
+    // Cover
+    // 1. status: false
+    // 2. message length:  0
+    // 3. token length:  > 0
+    [TestCase('Serialize with False and empty msg and non-empty token', 'False,,some token')]
+    procedure SerializeNonEmptyMsg(const status: Boolean; const msg: String; const token: String);
+
+    // procedure SerializeJustMessageIsSetToEmpty(const status: Boolean);
 
   end;
 
@@ -47,38 +58,28 @@ uses
   ObjectsMappers, System.JSON;
 
 procedure TTestActiveQueueResponceToJson.SerializeNonEmptyMsg(
-  const status: Boolean; const msg: String);
+  const status: Boolean; const msg: String; const Token: String);
 var
   obj: TActiveQueueResponce;
   jo: TJsonObject;
+  val1, val2: TJsonValue;
 begin
-  obj := TActiveQueueResponce.Create(Status, msg);
+  obj := TActiveQueueResponce.Create(Status, msg, Token);
   jo := Mapper.ObjectToJSONObject(obj);
   Assert.AreEqual((jo.GetValue('status') as TJsonBool).asBoolean, Status);
-  Assert.AreEqual(jo.GetValue('msgstat').value, msg);
-end;
+  val1 := jo.GetValue('msgstat');
+  val2 := jo.GetValue('token');
+  if msg = '' then
+    Assert.IsTrue((val1 = nil) OR (val1.Value = ''))
+  else
+    Assert.AreEqual(val1.value, msg);
 
-procedure TTestActiveQueueResponceToJson.Setup;
-begin
-end;
+  if Token = '' then
+    Assert.IsTrue((val2 = nil) OR (val2.Value = ''))
+  else
+    Assert.AreEqual(val2.value, Token);
 
-procedure TTestActiveQueueResponceToJson.TearDown;
-begin
 end;
-
-procedure TTestActiveQueueResponceToJson.SerializeJustMessageIsSetToEmpty(const status: Boolean);
-var
-  obj: TActiveQueueResponce;
-  jo: TJsonObject;
-  value: TJsonValue;
-begin
-  obj := TActiveQueueResponce.Create(Status, '');
-  jo := Mapper.ObjectToJSONObject(obj);
-  Assert.AreEqual((jo.GetValue('status') as TJsonBool).asBoolean, Status);
-  value := jo.GetValue('msgstat');
-  Assert.IsTrue((value = nil) OR (value.Value = ''));
-end;
-
 
 initialization
 
