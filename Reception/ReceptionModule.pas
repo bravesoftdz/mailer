2ce412d42cf931085ed9ad48bc288cc37381f051
Mario Rossi <veontomo@gmail.com>
2017-07-07 09:48:44 +0200

    Make the reception module use correct controller
    The IDE sucks at refactoring: once a name of the unit is changed (by
    means of Rename from the context menu), the IDE does not update the
    name in all usages.
    
    In case of Reception project, the renaming
    Controller -> ReceptionController has evidently been successful, but
    afterwards no methods in the controller were visible.
    This is due to the fact that ReceptionModule used Controller unit
    which after renaming should have become ReceptionController.
    Since there has been left its previous name, which now corresponds to the
    default controller, ReceptionController unit has become not used and hence
    none if its methods could be called.

--- Reception/ReceptionModule.pas
+++ Reception/ReceptionModule.pas
@@ -3,9 +3,9 @@ unit ReceptionModule;
 interface
 
 uses System.SysUtils,
-     System.Classes,
-     Web.HTTPApp,
-     MVCFramework;
+  System.Classes,
+  Web.HTTPApp,
+  MVCFramework;
 
 type
   TReceptionModule = class(TWebModule)
@@ -24,30 +24,31 @@ implementation
 
 {$R *.dfm}
 
-uses Controller, MVCFramework.Commons;
+
+uses ReceptionController, MVCFramework.Commons;
 
 procedure TReceptionModule.WebModuleCreate(Sender: TObject);
 begin
   FMVC := TMVCEngine.Create(Self,
     procedure(Config: TMVCConfig)
     begin
-      //enable static files
+      // enable static files
       Config[TMVCConfigKey.DocumentRoot] := ExtractFilePath(GetModuleName(HInstance)) + '\www';
       // session timeout (0 means session cookie)
       Config[TMVCConfigKey.SessionTimeout] := '0';
-      //default content-type
+      // default content-type
       Config[TMVCConfigKey.DefaultContentType] := TMVCConstants.DEFAULT_CONTENT_TYPE;
-      //default content charset
+      // default content charset
       Config[TMVCConfigKey.DefaultContentCharset] := TMVCConstants.DEFAULT_CONTENT_CHARSET;
-      //unhandled actions are permitted?
+      // unhandled actions are permitted?
       Config[TMVCConfigKey.AllowUnhandledAction] := 'false';
-      //default view file extension
+      // default view file extension
       Config[TMVCConfigKey.DefaultViewFileExtension] := 'html';
-      //view path
+      // view path
       Config[TMVCConfigKey.ViewPath] := 'templates';
-      //Enable STOMP messaging controller
+      // Enable STOMP messaging controller
       Config[TMVCConfigKey.Messaging] := 'false';
-      //Enable Server Signature in response
+      // Enable Server Signature in response
       Config[TMVCConfigKey.ExposeServerSignature] := 'true';
       // Define a default URL for requests that don't map to a route or a file (useful for client side web app)
       Config[TMVCConfigKey.FallbackResource] := 'index.html';
