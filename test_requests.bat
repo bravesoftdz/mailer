042accfc9908e2b95a309b5f9dcca48a63ea1770
Mario Rossi <veontomo@gmail.com>
2017-07-12 09:16:51 +0200

    Unstage a batch file that performs requests by means of curl

--- test_requests.bat
+++
@@ -1,6 +0,0 @@
-echo off
-REM ECHO "Simulate requests from the Reception server to the Dispatcher server" 
-REM FOR /L %%A IN (1,1,1) DO curl -X POST -H "Content-Type: multipart/form-data;charset=ASCII" -F data="{\"html\":\"html %%A\", \"text\":\"text %%A\", \"token\":\"super secret\"};type=application/json" -F file=@dumb.txt http://localhost/venditori/send
-
-ECHO "Simulate requests from the Reception server to the Dispatcher server" 
-FOR /L %%A IN (1,1,1) DO curl -X POST -H "Content-Type: multipart/form-data;charset=ASCII" -F data="{\"email\":\"a.shcherbakov@ritoll.it\", \"name\":\"Mario Rossi %%A\", \"phone\":\"12345-%%A\", \"country\":\"Italia\",\"token\":\"super secret\"};type=application/json" -F file=@dumb.txt http://localhost/onm/register
