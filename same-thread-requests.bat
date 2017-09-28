7360f8e4ee82566cfbb47473a336ccd41ce80ccb
Mario Rossi <veontomo@gmail.com>
2017-09-28 10:02:57 +0200

    Make the batch files send attached a realistic image

--- same-thread-requests.bat
+++ same-thread-requests.bat
@@ -7,7 +7,7 @@ IF "%requests%"=="" (
 ECHO Make %requests% requests to the Dispatcher server made one after another
 FOR /L %%A IN (1, 1, %requests%) DO (
  echo %%A 
- curl -X POST -H "Content-Type: multipart/form-data;charset=ASCII" -F data="{\"email\":\"a.shcherbakov@ritoll.it\", \"name\":\"Mario Rossi %%A\", \"phone\":\"%date% %time%\", \"country\":\"Italia\",\"token\":\"super secret\"};type=application/json" -F file1=@..\java.png -F file2=@dumb.txt http://localhost/onm/register
+ curl -X POST -H "Content-Type: multipart/form-data;charset=ASCII" -F data="{\"email\":\"a.shcherbakov@ritoll.it\", \"name\":\"Mario Rossi %%A\", \"phone\":\"%date% %time%\", \"country\":\"Italia\",\"token\":\"super secret\"};type=application/json" -F image.png=@..\java.png -F info.txt=@dumb.txt http://localhost/onm/register
  echo(
 )
 echo(
